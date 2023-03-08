cd35790495e22c27ffa53d7557a28c96c17d00dc
FIRAS <97522852+fifolio@users.noreply.github.com>
2023-03-08 20:05:34 +0100

    Delete todo directory

--- todo/app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const app = express();
-const port = 8000;
-app.listen(port, () => console.log(`Express started`));
-mongoose.connect("mongodb://localhost:27017/ToDo", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
