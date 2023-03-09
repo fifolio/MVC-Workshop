f10a98ba74798139ff3bd6664c5b8be2987e06b2
Firas <firasdabbabi@gmail.com>
2023-03-09 20:20:48 +0100

    version 1.2: Adding JSDoc files

---
+++ jsdoc-home.md
@@ -0,0 +1,12 @@
+# ToDo App
+
+### Welcome to the ToDo documentation 👋
+
+## Quick Start 🏃
+
+The contents of this document have been written to document and explain how some parts work in the project.
+This document may not contain the details of the entire project, but it covers more than 80% of its parts.
+
+Please email me if there is an error in writing a code or description at [FirasDabbabi@gmail.com](mailto:FirasDabbabi@gmail.com) , or contact me via my official [GitHub](https://github.com/fifolio/) and [LinkedIn](https://www.linkedin.com/in/fifolio/) accounts shown above,
+
+**Happy reading** ✌️
--- todo/app.js
+++ todo/app.js
@@ -1,7 +1,17 @@
-// Import Express & Mongoose
+/**
+ * @summary Import Express
+ * @this {express} used to import the Express framework.
+ */
 const express = require("express");
+/**
+ * @summary Import Mongoose
+ * @this {mongoose} used to import the Mongoose library.
+ */
 const mongoose = require("mongoose");
-// Pass Express() to App, and set the Server Port to 8000
+/**
+ * @summary Pass Express() to App, and set the Server Port to 8000
+ * @this {app} is an instant for Express framework.
+ */
 const app = express();
 const port = 8000;
 // Let the App Listen at Port 8000
@@ -19,17 +29,23 @@ mongoose
     console.log(`This is Err msg because of db connection: ${err}`)
   );
 
-// Create New Mongoose Schema {Record structure contains the default key and value type}
-const schema = new mongoose.Schema({ title: String, data: new Date() });
+/**
+ * @summary Create New Mongoose Schema {Record structure contains the default key and value type}
+ * @this {schema} is used to Create New Mongoose Schema
+ */
+const schema = new mongoose.Schema({ title: String });
 
-// Create New Model {Collection} that takes {"its Name", and its Data Schema structure}
+/**
+ * @summary Create New Model {Collection} that takes {"its Name", and its Data Schema structure}
+ * @this {Task} is used to Create New Model
+ */
 const Task = mongoose.model("Task", schema);
 
 // Set get() method to define a route that listen for "/create" path
 // Add callback function that gets executed when the server receives a the request
+
 app.get("/create", (req, res) => {
-  // insert Data to Database then confirm it with via console
-  // Catch the Errs if any
+  // Making new data that takes its (model) and (schema) from {Task} | Then call the .save method to Pass the data | and Catch any errs
   const firstTask = new Task({ title: "Send from express!" });
   firstTask
     .save()
